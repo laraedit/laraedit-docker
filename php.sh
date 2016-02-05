a5138aa8e3e139f9300182fc2d41f7e6dd995326
Derek Bourgeois <derek@ibourgeois.com>
2016-02-05 06:21:21 -0500

    Update php.sh

--- php.sh
+++ php.sh
@@ -28,24 +28,26 @@ sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.0/fpm
 sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.0/fpm/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/fpm/php.ini
 
-# cat > /etc/nginx/fastcgi_params << EOF
-# fastcgi_param	QUERY_STRING		\$query_string;
-# fastcgi_param	REQUEST_METHOD		\$request_method;
-# fastcgi_param	CONTENT_TYPE		\$content_type;
-# fastcgi_param	CONTENT_LENGTH		\$content_length;
-# fastcgi_param	SCRIPT_FILENAME		\$request_filename;
-# fastcgi_param	SCRIPT_NAME		\$fastcgi_script_name;
-# fastcgi_param	REQUEST_URI		\$request_uri;
-# fastcgi_param	DOCUMENT_URI		\$document_uri;
-# fastcgi_param	DOCUMENT_ROOT		\$document_root;
-# fastcgi_param	SERVER_PROTOCOL		\$server_protocol;
-# fastcgi_param	GATEWAY_INTERFACE	CGI/1.1;
-# fastcgi_param	SERVER_SOFTWARE		nginx/\$nginx_version;
-# fastcgi_param	REMOTE_ADDR		\$remote_addr;
-# fastcgi_param	REMOTE_PORT		\$remote_port;
-# fastcgi_param	SERVER_ADDR		\$server_addr;
-# fastcgi_param	SERVER_PORT		\$server_port;
-# fastcgi_param	SERVER_NAME		\$server_name;
-# fastcgi_param	HTTPS			\$https if_not_empty;
-# fastcgi_param	REDIRECT_STATUS		200;
-# EOF
+sed -i "s/;daemonize = yes/daemonize = no/g" /etc/php/7.0/fpm/php-fpm.conf
+
+cat > /etc/nginx/fastcgi_params << EOF
+fastcgi_param	QUERY_STRING		\$query_string;
+fastcgi_param	REQUEST_METHOD		\$request_method;
+fastcgi_param	CONTENT_TYPE		\$content_type;
+fastcgi_param	CONTENT_LENGTH		\$content_length;
+fastcgi_param	SCRIPT_FILENAME		\$request_filename;
+fastcgi_param	SCRIPT_NAME		\$fastcgi_script_name;
+fastcgi_param	REQUEST_URI		\$request_uri;
+fastcgi_param	DOCUMENT_URI		\$document_uri;
+fastcgi_param	DOCUMENT_ROOT		\$document_root;
+fastcgi_param	SERVER_PROTOCOL		\$server_protocol;
+fastcgi_param	GATEWAY_INTERFACE	CGI/1.1;
+fastcgi_param	SERVER_SOFTWARE		nginx/\$nginx_version;
+fastcgi_param	REMOTE_ADDR		\$remote_addr;
+fastcgi_param	REMOTE_PORT		\$remote_port;
+fastcgi_param	SERVER_ADDR		\$server_addr;
+fastcgi_param	SERVER_PORT		\$server_port;
+fastcgi_param	SERVER_NAME		\$server_name;
+fastcgi_param	HTTPS			\$https if_not_empty;
+fastcgi_param	REDIRECT_STATUS		200;
+EOF
