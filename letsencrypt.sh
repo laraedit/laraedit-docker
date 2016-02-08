a2a81d32f3986f6520bc973f8592be108c41bd3e
Derek Bourgeois <derek@ibourgeois.com>
2016-02-08 17:21:42 -0500

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ $EMAIL != "app@laraedit.com" && $DOMAIN != "app.dev" ]]; then
+if [[ $APP_EMAIL != "app@laraedit.com" && $APP_DOMAIN != "app.dev" ]]; then
 
 git clone https://github.com/letsencrypt/letsencrypt
 cd letsencrypt
 
 cat config.ini >  << EOF
 rsa-key-size = 4096
-email = $EMAIL
-domains = $DOMAIN
+email = $APP_EMAIL
+domains = $APP_DOMAIN
 authenticator = webroot
 webroot-path = /var/www/html/$APP_NAME
 EOF
